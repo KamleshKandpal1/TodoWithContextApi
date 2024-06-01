871a17136f0bdf856fa5c72358113a4706788d1c
Kamlesh Kandpal <kkandpal007@gmail.com>
2024-06-02 00:03:50 +0530

    latest-commit

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,12 @@ function App() {
   const [taskInput, setTaskInput] = useState("");
   const [updateTaskInput, setUpdateTaskInput] = useState("");
   const [updateId, setUpdateId] = useState("");
+
   const setEditTask = (id, task) => {
     setUpdateId(id);
     setUpdateTaskInput(task);
   };
+
   useEffect(() => {
     const loadTasks = JSON.parse(localStorage.getItem("tasks"));
     if (loadTasks && loadTasks.length > 0) {
@@ -31,114 +33,104 @@ function App() {
   }, [tasks]);
 
   return (
-    <>
-      <div className="w-full h-screen bg-[#a3989c] ">
-        <h1 className="p-[10px] text-center text-[40px] font-bold text-[#40383b]">
-          Todo List
-        </h1>
-        <div className="flex justify-center items-center flex-col ">
-          <div className="w-4/6">
-            <input
-              type="text"
-              name=""
-              id=""
-              placeholder="Add Task ....."
-              className="w-[80%] p-1 rounded-tl-md rounded-bl-md focus:outline-none"
-              value={taskInput}
-              onChange={(e) => setTaskInput(e.target.value)}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  addTasks(taskInput);
-                  setTaskInput("");
-                }
-              }}
-            />
-            <button
-              className="bg-[#40383b] w-[20%] p-1 text-white rounded-tr-md rounded-br-md"
-              onClick={() => {
+    <div className="w-full h-screen bg-[#a3989c] flex flex-col items-center justify-center">
+      <h1 className="p-4 text-center text-3xl font-bold text-[#40383b] md:text-4xl">
+        Todo List
+      </h1>
+      <div className="w-full max-w-md px-4">
+        <div className="flex mb-4">
+          <input
+            type="text"
+            placeholder="Add Task ....."
+            className="flex-1 p-2 rounded-tl-md rounded-bl-md focus:outline-none"
+            value={taskInput}
+            onChange={(e) => setTaskInput(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
                 addTasks(taskInput);
                 setTaskInput("");
-              }}
-            >
-              Add
-            </button>
-          </div>
-          <ul className="w-4/6 py-1.5">
-            {tasks.map((task, index) => (
-              <li
-                key={index}
-                className="odd:bg-[#a3989c] even:bg-[#8b7cb1] odd:text-[#c1bdbc] even:text-[#a19698] px-1 py-[5px] my-1 rounded-md flex items-center "
-              >
-                {updateId !== index && (
-                  <input
-                    type="checkbox"
-                    name=""
-                    className="w-[4%]"
-                    onChange={(e) => setStatusTask(index, e.target.checked)}
-                    checked={task.complete}
-                  />
-                )}
-                {updateId === index ? (
-                  <input
-                    type="text"
-                    name=""
-                    id=""
-                    placeholder="Add Task ....."
-                    className="w-[90%] p-1 rounded-md  focus:outline-none"
-                    value={updateTaskInput}
-                    onChange={(e) => setUpdateTaskInput(e.target.value)}
-                    onKeyDown={(e) => {
-                      if (e.key === "Enter") {
-                        setUpdateTask(index, updateTaskInput);
-                        setUpdateId("");
-                        setUpdateTaskInput("");
-                      }
-                    }}
-                  />
-                ) : (
-                  <span
-                    className={`px-4 text-[15px] w-[86%] ${
-                      task.complete ? "line-through" : ""
-                    }`}
-                  >
-                    {task.task}
-                  </span>
-                )}
-
-                <div className="flex w-[10%] justify-around">
-                  {updateId === index ? (
-                    <button
-                      className="bg-green-500 w-[20px] h-[20px] flex items-center justify-center text-white rounded-sm"
-                      onClick={() => {
-                        setUpdateTask(index, updateTaskInput);
-                        setUpdateId("");
-                        setUpdateTaskInput("");
-                      }}
-                    >
-                      <FontAwesomeIcon icon={faCheck} />
-                    </button>
-                  ) : (
-                    <button
-                      className="bg-blue-500 w-[20px] h-[20px] flex items-center justify-center text-white rounded-sm"
-                      onClick={() => setEditTask(index, task.task)}
-                      disabled={task.complete}
-                    >
-                      <FontAwesomeIcon icon={faPen} />
-                    </button>
-                  )}
-                  <button
-                    className="bg-red-500 w-[20px] h-[20px] flex items-center justify-center text-white rounded-sm"
-                    onClick={() => setDeleteTask(index)}
-                  >
-                    <FontAwesomeIcon icon={faXmark} />
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
+              }
+            }}
+          />
+          <button
+            className="bg-[#40383b] p-2 text-white rounded-tr-md rounded-br-md"
+            onClick={() => {
+              addTasks(taskInput);
+              setTaskInput("");
+            }}
+          >
+            Add
+          </button>
         </div>
+        <ul className="space-y-2">
+          {tasks.map((task, index) => (
+            <li
+              key={index}
+              className="bg-[#a3989c] odd:bg-[#a3989c] even:bg-[#8b7cb1] odd:text-[#c1bdbc] even:text-[#a19698] px-4 py-2 rounded-md flex items-center"
+            >
+              {updateId !== index && (
+                <input
+                  type="checkbox"
+                  className="mr-2"
+                  onChange={(e) => setStatusTask(index, e.target.checked)}
+                  checked={task.complete}
+                />
+              )}
+              {updateId === index ? (
+                <input
+                  type="text"
+                  placeholder="Update Task ....."
+                  className="flex-1 p-2 rounded-md focus:outline-none"
+                  value={updateTaskInput}
+                  onChange={(e) => setUpdateTaskInput(e.target.value)}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      setUpdateTask(index, updateTaskInput);
+                      setUpdateId("");
+                      setUpdateTaskInput("");
+                    }
+                  }}
+                />
+              ) : (
+                <span
+                  className={`flex-1 ${task.complete ? "line-through" : ""}`}
+                >
+                  {task.task}
+                </span>
+              )}
+              <div className="flex space-x-2">
+                {updateId === index ? (
+                  <button
+                    className="bg-green-500 w-8 h-8 flex items-center justify-center text-white rounded-sm"
+                    onClick={() => {
+                      setUpdateTask(index, updateTaskInput);
+                      setUpdateId("");
+                      setUpdateTaskInput("");
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faCheck} />
+                  </button>
+                ) : (
+                  <button
+                    className="bg-blue-500 w-8 h-8 flex items-center justify-center text-white rounded-sm"
+                    onClick={() => setEditTask(index, task.task)}
+                    disabled={task.complete}
+                  >
+                    <FontAwesomeIcon icon={faPen} />
+                  </button>
+                )}
+                <button
+                  className="bg-red-500 w-8 h-8 flex items-center justify-center text-white rounded-sm"
+                  onClick={() => setDeleteTask(index)}
+                >
+                  <FontAwesomeIcon icon={faXmark} />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
-    </>
+    </div>
   );
 }
 
